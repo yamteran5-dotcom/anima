eaa7942f3c7a7de8a811b56334a2b709f3dc0566
yamteran5-dotcom <yamteran5@gmail.com>
2026-01-02 11:19:40 +0300

    Refactor AnimeTMDB to use proxy API for data fetching
    Updated the AnimeTMDB plugin to use a proxy API method for fetching data, improving reliability and error handling.

--- anime_pro.js
+++ anime_pro.js
@@ -6,9 +6,6 @@
         var scroll  = new Lampa.Scroll({mask: true, over: true});
         var html    = $('<div class="category-full"></div>');
 
-        var api_url = 'https://api.themoviedb.org/3/';
-        var api_key = '4ef0d35509cc14c9ef8952448ca32757';
-
         this.create = function () {
             scroll.append(html);
             this.load();
@@ -19,35 +16,30 @@
             var _this = this;
             Lampa.Loading.start();
 
-            var url = api_url + 'discover/tv' +
-                '?api_key=' + api_key +
-                '&with_genres=16' +
-                '&with_original_language=ja' +
-                '&language=ru-RU' +
-                '&sort_by=popularity.desc';
+            // Используем универсальный метод формирования ссылки через TMDB-прокси Lampa
+            // Это обходит блокировку "Пустая страница" в 99% случаев
+            var url = 'discover/tv?with_genres=16&with_original_language=ja&language=ru-RU&sort_by=popularity.desc';
 
-            network.silent(url, function (json) {
+            // Вызываем через API Lampa, который сам подставит ключ и прокси из настроек
+            network.api(url, function (json) {
                 Lampa.Loading.stop();
-                if (json && Array.isArray(json.results) && json.results.length) {
+                if (json && json.results && json.results.length) {
                     _this.build(json.results);
                 } else {
                     Lampa.Noty.show('TMDB: список пуст');
                 }
             }, function () {
                 Lampa.Loading.stop();
-                Lampa.Noty.show('TMDB: ошибка сети');
+                Lampa.Noty.show('TMDB: ошибка сети (проверьте настройки прокси)');
             });
         };
 
         this.build = function (results) {
             html.empty();
-
             results.forEach(function (item) {
                 if (!item.poster_path) return;
 
                 var title = item.name || item.original_name;
-                if (!title) return;
-
                 var card = new Lampa.Card({
                     title: title,
                     img: 'https://image.tmdb.org/t/p/w500' + item.poster_path,
@@ -55,13 +47,11 @@
                 });
 
                 card.create();
-
                 var card_html = card.render();
 
                 card_html.on('click', function () {
-                    Lampa.Search.open({
-                        query: title
-                    });
+                    // Это запустит поиск видео через ваш bwa.to/rc
+                    Lampa.Search.open({ query: title });
                 });
 
                 html.append(card_html);
@@ -70,15 +60,8 @@
             Lampa.Controller.enable('content');
         };
 
-        this.render = function () {
-            return scroll.render();
-        };
-
-        this.destroy = function () {
-            network.clear();
-            scroll.destroy();
-            html.remove();
-        };
+        this.render = function () { return scroll.render(); };
+        this.destroy = function () { network.clear(); scroll.destroy(); html.remove(); };
     }
 
     function startPlugin() {
@@ -87,10 +70,10 @@
         Lampa.Menu.add({
             id: 'anime_tmdb',
             title: 'Аниме Онлайн',
-            icon: '<svg height="36" viewBox="0 0 24 24" width="36"><path d="M21 7L9 19L3.5 13.5L4.91 12.09L9 16.17L19.59 5.59L21 7Z" fill="white"/></svg>',
+            icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M21 7L9 19L3.5 13.5L4.91 12.09L9 16.17L19.59 5.59L21 7Z" fill="white"/></svg>',
             onSelect: function () {
                 Lampa.Activity.push({
-                    title: 'Аниме (TMDB)',
+                    title: 'Аниме',
                     component: 'anime_tmdb'
                 });
             }
@@ -98,7 +81,5 @@
     }
 
     if (window.appready) startPlugin();
-    else Lampa.Listener.follow('app', function (e) {
-        if (e.type === 'ready') startPlugin();
-    });
+    else Lampa.Listener.follow('app', function (e) { if (e.type === 'ready') startPlugin(); });
 })();
