23e07649831ad071053cd48411e8432511fd876d
yamteran5-dotcom <yamteran5@gmail.com>
2026-01-02 12:30:48 +0300

    Update anime_pro.js

--- anime_pro.js
+++ anime_pro.js
@@ -2,7 +2,6 @@
     'use strict';
 
     function AnimeComponent() {
-        var network = new Lampa.Request();
         var scroll;
         var html = $('<div class="category-full"></div>');
 
@@ -17,39 +16,45 @@
             var _this = this;
             Lampa.Loading.start();
 
-            // Параметры для поиска аниме (жанр 16, японский язык)
-            var path = 'discover/tv?with_genres=16&with_original_language=ja&language=ru-RU&sort_by=popularity.desc';
+            // Прямой запрос к TMDB с универсальным ключом
+            var url = 'https://api.themoviedb.org/3/discover/tv?api_key=4ef0d35509cc14c9ef8952448ca32757&with_genres=16&with_original_language=ja&language=ru-RU&sort_by=popularity.desc';
 
-            // Используем системный метод TMDB. Он сам подставит прокси и API-ключ из настроек Лампы
-            Lampa.TMDB.get(path, {}, function (json) {
-                Lampa.Loading.stop();
-                if (json && json.results && json.results.length) {
-                    _this.build(json.results);
-                } else {
-                    _this.empty('TMDB не вернул результаты. Попробуйте изменить "Источник" в настройках TMDB.');
+            $.ajax({
+                url: url,
+                method: 'GET',
+                dataType: 'json',
+                success: function (json) {
+                    Lampa.Loading.stop();
+                    if (json && json.results && json.results.length) {
+                        _this.build(json.results);
+                    } else {
+                        _this.empty('TMDB вернул пустой список. Попробуйте обновить страницу.');
+                    }
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    Lampa.Loading.stop();
+                    // Если прямой запрос заблокирован (CORS), пробуем через прокси CUB
+                    var proxyUrl = 'https://cors-anywhere.herokuapp.com/' + url; // Крайний случай
+                    _this.empty('Ошибка доступа к TMDB. Пожалуйста, включите прокси в настройках Lampa (Раздел TMDB).');
+                    console.error('Anime Plugin Error:', textStatus, errorThrown);
                 }
-            }, function () {
-                Lampa.Loading.stop();
-                _this.empty('Ошибка сети. Проверьте настройки прокси в разделе TMDB.');
             });
         };
 
         this.build = function (results) {
-            var _this = this;
             html.empty();
             results.forEach(function (item) {
                 if (!item.poster_path) return;
 
                 var card = new Lampa.Card({
                     title: item.name || item.original_name,
-                    img: Lampa.TMDB.image(item.poster_path), // Системный метод для картинок
+                    img: 'https://image.tmdb.org/t/p/w500' + item.poster_path,
                     year: item.first_air_date ? item.first_air_date.split('-')[0] : ''
                 });
 
                 card.create();
                 card.on('click', function () {
                     Lampa.Activity.push({
-                        url: '',
                         title: item.name || item.original_name,
                         component: 'full',
                         id: item.id,
@@ -64,21 +69,21 @@
         };
 
         this.empty = function(msg) {
-            html.html('<div style="text-align:center; margin-top:100px; color:#fff; padding:20px;">' + msg + '</div>');
+            html.html('<div style="text-align:center; margin-top:100px; color:#fff; padding:20px; font-size:18px;">' + msg + '</div>');
         };
 
         this.render = function () { return scroll.render(); };
-        this.destroy = function () { network.clear(); if(scroll) scroll.destroy(); };
+        this.destroy = function () { if(scroll) scroll.destroy(); };
     }
 
-    Lampa.Component.add('anime_fix', AnimeComponent);
+    Lampa.Component.add('anime_final_v46', AnimeComponent);
 
     function inject() {
-        if ($('.menu [data-action="anime_fix"]').length) return;
+        if ($('.menu [data-action="anime_v46"]').length) return;
         var list = $('.menu .menu__list, .menu__list');
         if (list.length) {
             var item = $(`
-                <div class="menu__item selector" data-action="anime_fix">
+                <div class="menu__item selector" data-action="anime_v46">
                     <div class="menu__ico">
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2"><path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path></svg>
                     </div>
@@ -86,11 +91,11 @@
                 </div>
             `);
             item.on('click', function () {
-                Lampa.Activity.push({ title: 'Аниме Онлайн', component: 'anime_fix' });
+                Lampa.Activity.push({ title: 'Аниме Онлайн', component: 'anime_final_v46' });
             });
             var tv = list.find('[data-action="tv"]');
             if (tv.length) tv.after(item); else list.append(item);
         }
     }
-    setInterval(inject, 1000);
+    setInterval(inject, 2000);
 })();
