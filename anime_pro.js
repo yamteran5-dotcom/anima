9bd3020820c2b9d3caf8444d0aa53a9c66eeeb8f
yamteran5-dotcom <yamteran5@gmail.com>
2026-01-02 11:49:37 +0300

    Refactor AnimeTMDB component and menu integration
    Refactor AnimeTMDB component and initialization logic.

--- anime_pro.js
+++ anime_pro.js
@@ -1,6 +1,7 @@
 (function () {
     'use strict';
 
+    // 1. Описываем сам компонент (каталог аниме)
     function AnimeTMDB() {
         var network = new Lampa.Request();
         var scroll;
@@ -16,13 +17,13 @@
         this.load = function () {
             var _this = this;
             Lampa.Loading.start();
-            // Используем нативный API TMDB через прокси Лампы
+            // Запрос через встроенный метод API
             network.api('discover/tv?with_genres=16&with_original_language=ja&language=ru-RU&sort_by=popularity.desc', function (json) {
                 Lampa.Loading.stop();
                 if (json && json.results) _this.build(json.results);
             }, function () {
                 Lampa.Loading.stop();
-                Lampa.Noty.show('Ошибка загрузки Аниме');
+                Lampa.Noty.show('Ошибка загрузки данных');
             });
         };
 
@@ -43,46 +44,38 @@
             Lampa.Controller.enable('content');
         };
 
-        this.render = function () { return scroll.render(); };
-        this.destroy = function () { network.clear(); scroll.destroy(); html.remove(); };
+        this.render = function () { return scroll ? scroll.render() : $('<div></div>'); };
+        this.destroy = function () { network.clear(); if(scroll) scroll.destroy(); };
     }
 
-    // ГЛАВНОЕ: Гарантированная вставка в меню
-    function init() {
-        if (window.anime_plugin_loaded) return;
-
+    // 2. Функция инициализации
+    function startPlugin() {
+        // Регистрация компонента
         Lampa.Component.add('anime_tmdb', AnimeTMDB);
 
-        // Пытаемся добавить в меню несколько раз, пока не получится
-        var timer = setInterval(function() {
-            if (typeof Lampa !== 'undefined' && Lampa.Menu) {
-                var menu_item = {
-                    id: 'anime_tmdb',
-                    title: 'Аниме Онлайн',
-                    icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M21 7L9 19L3.5 13.5L4.91 12.09L9 16.17L19.59 5.59L21 7Z" fill="#ff3e3e"/></svg>',
-                    onSelect: function () {
-                        Lampa.Activity.push({
-                            title: 'Аниме',
-                            component: 'anime_tmdb'
-                        });
-                    }
-                };
-
-                // Проверяем, нет ли уже такого пункта, чтобы не дублировать
-                if (!$('.menu [data-id="anime_tmdb"]').length) {
-                    Lampa.Menu.add(menu_item);
-                }
-                
-                window.anime_plugin_loaded = true;
-                clearInterval(timer); // Останавливаем цикл, когда добавили
+        // Регистрация в меню через официальный метод
+        var menu_item = {
+            id: 'anime_tmdb',
+            title: 'Аниме Онлайн',
+            icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M21 7L9 19L3.5 13.5L4.91 12.09L9 16.17L19.59 5.59L21 7Z" fill="white"/></svg>',
+            onSelect: function () {
+                Lampa.Activity.push({
+                    title: 'Аниме',
+                    component: 'anime_tmdb'
+                });
             }
-        }, 500);
-
-        // Страховка: если через 10 секунд не добавилось, прекращаем
-        setTimeout(function() { clearInterval(timer); }, 10000);
+        };
+        
+        // Добавляем пункт в меню
+        Lampa.Menu.add(menu_item);
     }
 
-    // Запускаем немедленно
-    init();
-
+    // 3. Правильный запуск без создания "зависаний"
+    if (window.appready) {
+        startPlugin();
+    } else {
+        Lampa.Listener.follow('app', function (e) {
+            if (e.type == 'ready') startPlugin();
+        });
+    }
 })();
