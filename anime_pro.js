8833192ba07c04a3cdf4092ecc2eb08e6622e8de
yamteran5-dotcom <yamteran5@gmail.com>
2026-01-02 12:23:52 +0300

    Update anime_pro.js

--- anime_pro.js
+++ anime_pro.js
@@ -1,82 +1,67 @@
-(function () {
+(function(){
     'use strict';
 
-    function AnimeComponent() {
+    function AnimeComponent(){
         var network = new Lampa.Request();
-        var scroll;
+        var scroll = new Lampa.Scroll({mask:true, over:true});
         var html = $('<div class="category-full"></div>');
+        scroll.append(html);
 
-        this.create = function () {
-            scroll = new Lampa.Scroll({mask: true, over: true});
-            scroll.append(html);
+        this.create = function(){
+            Lampa.Loading.start();
             this.load();
             return scroll.render();
         };
 
-        this.load = function () {
+        this.load = function(){
             var _this = this;
-            Lampa.Loading.start();
-
-            // Пытаемся сформировать URL через системные прокси Lampa
-            // Если network.api в вашей сборке Lampac пустой, используем прямую ссылку
-            var url = 'https://api.themoviedb.org/3/discover/tv?api_key=4ef0d35509cc14c9ef8952448ca32757&with_genres=16&with_original_language=ja&language=ru-RU&sort_by=popularity.desc';
-
-            // Используем стандартный network.silent для обхода внутренних фильтров
-            network.silent(url, function (json) {
+            // Используем встроенный network.api (Lampac прокси)
+            network.api('discover/tv?with_genres=16&with_original_language=ja&language=ru-RU', function(json){
                 Lampa.Loading.stop();
-                if (json && json.results && json.results.length) {
-                    _this.build(json.results);
-                } else {
-                    _this.empty('TMDB не вернул данные. Попробуйте сменить прокси в настройках.');
+                if(!json || !json.results || !json.results.length){
+                    html.html('<div style="text-align:center; margin-top:50px; color:#fff;">Список пуст</div>');
+                    return;
                 }
-            }, function () {
+                _this.build(json.results);
+            }, function(){
                 Lampa.Loading.stop();
-                _this.empty('Ошибка сети: Lampac блокирует прямой запрос к TMDB.');
+                html.html('<div style="text-align:center; margin-top:50px; color:#fff;">Ошибка сети</div>');
             });
         };
 
-        this.build = function (results) {
-            var _this = this;
+        this.build = function(results){
             html.empty();
-
-            results.forEach(function (item) {
-                if (!item.poster_path) return;
-
+            results.forEach(function(item){
+                if(!item.poster_path) return;
                 var card = new Lampa.Card({
                     title: item.name || item.original_name,
                     img: 'https://image.tmdb.org/t/p/w500' + item.poster_path,
                     year: item.first_air_date ? item.first_air_date.split('-')[0] : ''
                 });
-
                 card.create();
-                card.on('click', function () {
-                    Lampa.Search.open({ query: item.name || item.original_name });
+                card.on('click', function(){
+                    Lampa.Search.open({query:item.name || item.original_name});
                 });
-
                 html.append(card.render());
             });
-
             Lampa.Controller.enable('content');
         };
 
-        this.empty = function(msg) {
-            html.html('<div style="text-align:center; margin-top:100px; color:#fff; font-size:1.5em; padding:20px;">' + msg + '</div>');
-        };
-
-        this.render = function () { return scroll.render(); };
-        this.destroy = function () { network.clear(); if(scroll) scroll.destroy(); html.remove(); };
+        this.render = function(){ return scroll.render(); };
+        this.destroy = function(){ network.clear(); scroll.destroy(); html.remove(); };
     }
 
-    // Регистрация компонента
     Lampa.Component.add('anime_final_v42', AnimeComponent);
 
-    // Функция вставки в меню
-    function inject() {
-        if ($('.menu [data-action="anime_v42"]').length) return;
-        var menuList = $('.menu .menu__list, .menu__list');
-        if (menuList.length) {
+    // Добавление меню один раз после ready
+    Lampa.Listener.follow('menu', function(e){
+        if(e.type==='ready'){
+            if($('.menu [data-action="anime_final_v42"]').length) return;
+            var menuList = $('.menu .menu__list');
+            if(!menuList.length) return;
+
             var menu_item = $(`
-                <div class="menu__item selector" data-action="anime_v42">
+                <div class="menu__item selector" data-action="anime_final_v42">
                     <div class="menu__ico">
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2">
                             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
@@ -86,20 +71,14 @@
                 </div>
             `);
 
-            menu_item.on('click', function () {
-                Lampa.Activity.push({
-                    title: 'Аниме Онлайн',
-                    component: 'anime_final_v42'
-                });
+            menu_item.on('click', function(){
+                Lampa.Activity.push({title:'Аниме Онлайн', component:'anime_final_v42'});
             });
 
             var tv = menuList.find('[data-action="tv"]');
-            if (tv.length) tv.after(menu_item);
+            if(tv.length) tv.after(menu_item);
             else menuList.append(menu_item);
         }
-    }
-
-    // Постоянный контроль меню
-    setInterval(inject, 1500);
+    });
 
 })();
